bcd05301f93787f4e5acb2431a569b7c00c7b483
sameer rasa <crazzywizard@gmail.com>
2024-02-24 17:36:25 -0700

    removing cron job from fast api

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 import json
-import requests
 import aioredis
 from dotenv import load_dotenv
 import os
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 load_dotenv()
 
@@ -26,27 +24,8 @@ async def get_redis():
     )
 
 
-async def get_data():
-    redis = await get_redis()
-    data_1 = requests.get("https://api.quickindexer.xyz/leaderboard/?days=1").json()
-    data_7 = requests.get("https://api.quickindexer.xyz/leaderboard/?days=7").json()
-    data_30 = requests.get("https://api.quickindexer.xyz/leaderboard/?days=30").json()
-    data_from_api = [data_1, data_7, data_30]
-    print(data_from_api)
-    Leaderboard_data = {
-        "1": data_from_api[0],
-        "7": data_from_api[1],
-        "30": data_from_api[2],
-    }
-    await redis.set("leaderboard_data", json.dumps(Leaderboard_data))
-    await redis.close()
-
-
 @app.on_event("startup")
 async def startup():
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(get_data, "interval", minutes=5)
-    scheduler.start()
     print("App started")
 
 
